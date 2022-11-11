8705a05bd411945a1dd814b9f42fb8b5f3985967
Sofia <sofiarivas1993@gmail.com>
2022-11-11 12:12:23 -0300

    RFT-23 Edit data mock

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,7 +6,7 @@ const Header = (props) => {
     return (
         <div className={`d-flex justify-content-between align-items-center ${classes.header}`}>
             <a className={classes.logo}>{props.title}</a>
-            <button className="btn btn-outline-dark"  onClick={} title="Cart">
+            <button className="btn btn-outline-dark" title="Cart">
                 <FaShoppingCart/>
                 {props.listCart.length > 0 &&
                     <span className="badge bg-danger ms-2 text-white">{props.listCart.length}</span>}
